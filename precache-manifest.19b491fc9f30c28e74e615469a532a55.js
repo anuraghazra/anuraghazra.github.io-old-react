b9a4fd9eb8388fd0ba40344f0b593d9d97289340
anuraghazra <hazru.anurag@gmail.com>
2019-02-28 22:27:03 +0530

    Updates

---
+++ precache-manifest.19b491fc9f30c28e74e615469a532a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "18f1e46457243bcce64a",
+    "url": "/static/js/main.18f1e464.chunk.js"
+  },
+  {
+    "revision": "dfe0b679732f5b0076bb",
+    "url": "/static/js/2.dfe0b679.chunk.js"
+  },
+  {
+    "revision": "18f1e46457243bcce64a",
+    "url": "/static/css/main.de1cc52d.chunk.css"
+  },
+  {
+    "revision": "dfe0b679732f5b0076bb",
+    "url": "/static/css/2.e963c3bd.chunk.css"
+  },
+  {
+    "revision": "b7ac8df8f56394267e7cc05e6e670c95",
+    "url": "/index.html"
+  }
+];
